cfcd9af1e767ceecce0ba784f6a616d5ae8aaeba
ai_ionascu <ionutionascu27@yahoo.com>
2023-01-19 22:21:48 +0100

    functionality scripts pushed in separate folder

---
+++ logic/register.php
@@ -0,0 +1,146 @@
+<?php
+
+session_start();
+
+include('../conf/db_con.php');
+
+function send_email($email, $token){
+    include('send_verif_email.php');
+}
+
+if(isset($_POST['send_code']))
+{   
+    if(empty($_POST['email'])){
+        $_SESSION['email_err'] = "Email is required.";
+        header("Location: ../register_page.php");
+    }
+    else{
+        $email = $_POST['email'];
+
+        # verify if email exists
+        $email_existing_query = "SELECT * FROM users WHERE email='$email' LIMIT 1";
+        $email_existing_query_run = mysqli_query($con, $email_existing_query);
+
+        if(mysqli_num_rows($email_existing_query_run) > 0)
+        {
+            # add message into session variable
+            $_SESSION['error'] = "This email already exists.";
+            header("Location: ../register_page.php");
+        }
+        else
+        {   
+            # generate verification token
+            $token = rand(100000, 999999);
+
+            # send token email
+            send_email("$email", "$token");
+
+            # save user data into user session data
+            $_SESSION['email'] = $email;
+            $_SESSION['token'] = $token;
+            $_SESSION['success'] = "Verification code sent. Please check your inbox.";
+
+            # hide email verification form and make token verification form visible
+            $_SESSION['visibility'] = array(0,1,0);
+            header("Location: ../register_page.php");
+        }
+    }
+
+}
+
+if(isset($_POST['verify_code']))
+{
+    if(empty($_POST['verification_code'])){
+        $_SESSION['code_err'] = "Verification code is required.";
+        $_SESSION['visibility'] = array(0,1,0);
+        header("Location: ../register_page.php");
+    }
+    else{
+        $verification_code = $_POST['verification_code'];
+        if($verification_code != $_SESSION['token']){
+            # add message into session variable
+            $_SESSION['error'] = "Wrong token.";
+            $_SESSION['visibility'] = array(0,1,0);
+            header("Location: ../register_page.php");
+        }
+        else
+        {
+            # save user data into user session data
+            $_SESSION['success'] = "Code successfully verified.";
+            $_SESSION['visibility'] = array(0,0,1);
+            header("Location: ../register_page.php");
+        }
+    }
+}
+
+if(isset($_POST['reg_btn'])){
+    if(empty($_POST['username'])){
+        $errors['username_err'] = "A username is required.";
+    }
+    else{
+        $username = $_POST['username'];
+        # verify if username exists
+        $username_existing_query = "SELECT * FROM users WHERE username='$username' LIMIT 1";
+        $username_existing_query_run = mysqli_query($con, $username_existing_query); 
+        if(mysqli_num_rows($username_existing_query_run) > 0)
+        {
+            $errors['username_err'] = "This username already exists.";
+        } 
+    }
+ 
+    if(empty($_POST['email_reg']) || $_POST['email_reg'] != $_SESSION['email']){
+        $_SESSION['error'] = "Email not verified.";
+    }
+    else{
+        $email = $_POST['email_reg'];
+    }
+
+    if(empty($_POST['password'])){
+        $errors['pass_err'] = "Password is required.";
+    }
+    else if(empty($_POST['repeat_password']) || $_POST['repeat_password'] != $_POST['password']){
+        $errors['repass_err'] = "Passwords do not match.";
+    }
+    else{
+        $password = crypt($_POST['password'], 'something');
+    }
+
+    if(empty($_POST['usertype'])){
+        $errors['usertype_err'] = "Invalid user type selection.";
+    }
+    else{
+        $role = $_POST['usertype'];
+    }
+
+    if(!empty($_SESSION['error'])){
+        unset($_SESSION['visibility']);
+        header("Location: ../register_page.php");
+    }
+    else if(isset($errors)){
+        $_SESSION['reg_errors'] = $errors;
+        $_SESSION['visibility'] = array(0,0,1);
+        header("Location: ../register_page.php");
+    }
+    else{
+        // insert new user into database
+        if ($role == 'user'){
+        $register_query = "INSERT INTO users (username, email, password, role, validated) 
+                            VALUES('$username', '$email', '$password', '$role', 'yes')";
+        }
+        else{
+            $register_query = "INSERT INTO users (username, email, password, role) 
+                                VALUES('$username', '$email', '$password', '$role')";
+        }
+        $register_query_con = mysqli_query($con, $register_query);
+
+        // grant the 'user_role' role to the new user
+        $assign_role = "GRANT user_role TO '$username'@'%'";
+        $assign_role_con = mysqli_query($con, $assign_role);
+
+        $_SESSION['username'] = $username;
+        $_SESSION['success'] = 'You have been successfully registered.';
+        header('Location: ../index.php');
+    }
+}
+
+?>
---
+++ logic/signin.php
@@ -0,0 +1,48 @@
+<?php
+
+session_start();
+
+include('../conf/db_con.php');
+
+if(isset($_POST['login'])){
+    $login_data = mysqli_real_escape_string($con, $_POST['user_email']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+
+    if(empty($login_data)){
+        $login_errors['user_email'] = "Username or email required.";
+    }
+    if(empty($password)){
+        $login_errors['password'] = "Password required.";
+    }
+
+    if(empty($login_errors)){
+        $password = crypt($password, 'something');
+        $query_user = "SELECT * FROM users WHERE username = '$login_data' AND password = '$password'";
+        $query_email = "SELECT * FROM users WHERE email = '$login_data' AND password = '$password'";
+        $user_res = mysqli_query($con, $query_user);
+        $email_res = mysqli_query($con, $query_email);
+
+        if(mysqli_num_rows($user_res) == 1 || mysqli_num_rows($email_res) == 1){
+            if(mysqli_num_rows($user_res) == 1){
+                $user_info = mysqli_fetch_assoc($user_res);
+            }
+            if(mysqli_num_rows($email_res) == 1){
+                $user_info = mysqli_fetch_assoc($email_res);
+            }
+            $_SESSION['username'] = $user_info['username'];
+            $_SESSION['email'] = $user_info['email'];
+            $_SESSION['role'] = $user_info['role'];
+            $_SESSION['success'] = 'You have successfully signed in.';
+            header('Location: ../index.php');
+        }
+        else{
+            $_SESSION['error'] = 'Incorrect login information.';
+            header('Location: ./login_page.php');
+        }
+    }
+    else{
+        $_SESSION['login_errors'] = $login_errors;
+        header('Location: ./login_page.php');
+    }
+}
+?>
