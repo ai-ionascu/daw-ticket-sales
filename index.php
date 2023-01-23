f362b4007f4b88058e2e9407f10bd007e356006c
ai_ionascu <ionutionascu27@yahoo.com>
2023-01-23 18:45:59 +0100

    changed homepage

--- index.php
+++ index.php
@@ -9,16 +9,6 @@ include(dirname(__FILE__).'/conf/db_con.php');
 <h1>Home Page</h1>
 
 <?php
-$users_query = 'SELECT * FROM users';
-$users_query_run = mysqli_query($con, $users_query);
-if ($users_query_run){
-    foreach($users_query_run as $user){
-        print_r($user);
-    }
-}
-else{
-    echo "Query error.";
-}
 include('include/alerts.php');
 include('include/tail.php') 
 ?>
