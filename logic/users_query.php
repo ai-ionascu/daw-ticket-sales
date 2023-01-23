07465306f9ae3d3242b67236fa9239863933da6c
ai_ionascu <ionutionascu27@yahoo.com>
2023-01-23 23:21:47 +0100

    updated panel for admin users

--- logic/users_query.php
+++ logic/users_query.php
@@ -8,7 +8,14 @@ $items_per_page = 4;
 $num_pages = ceil($users_count/$items_per_page);
 
 if (isset($_GET['page'])){
-    $page = $_GET['page'];
+    $page = (int) $_GET['page'];
+}
+else{
+    $page = 1;
+}
+
+if (isset($_GET['usr'])){
+    $user_update = $_GET['usr'];
 }
 
 if (!isset($page) || $page == '1'){
@@ -28,12 +35,12 @@ $users_page = mysqli_query($con, "SELECT * FROM users LIMIT $start,$items_per_pa
         <table class="table">
             <thead class="table-dark">
             <tr>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Password</th>
-                <th>Role</th>
-                <th>Verified</th>
-                <th>Actions</th>
+                <th class="text-center">Username</th>
+                <th class="text-center">Email</th>
+                <th class="text-center">Password</th>
+                <th class="text-center">Role</th>
+                <th class="text-center">Verified</th>
+                <th class="text-center">Actions</th>
             </tr>
             </thead>
             <tbody class="table-border-bottom-0">
@@ -44,39 +51,112 @@ while ($row = mysqli_fetch_array($users_page)){
     echo '<tr><td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>'.$row['username'].'</strong></td>';
     echo '<td>'.$row['email'].'</td>';
     echo '<td>'.$row['password'].'</td>';
-    echo '<td>'.$row['role'].'</td>';
-    echo '<td>'.$row['verified'].'</td>';
-    ?>
+
+    if (isset($_GET['update'])){ 
+        ?>
+            <form action="logic/update_user.php" method="post">
+                <div class="form-group mb-3">
                     <td>
-                        <div class="dropdown">
-                            <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
-                            <i class="bi bi-three-dots-vertical"></i>
-                            </button>
-                            <div class="dropdown-menu">
+                        <select name="role" id="role">
                             <?php
-                            if (empty($row['verified'])){
-                                echo 
-                                    '<a class="dropdown-item" href=""
-                                    ><i class="bx bx-edit-alt me-1"></i> Validate</a>';
-                                if ($row['role'] != 'user'){
-                                    echo
-                                        '<a class="dropdown-item" href=""
-                                        ><i class="bx bx-edit-alt me-1"></i> Reset Role</a>';
+                                $roles = array('admin', 'employee', 'user');
+                                foreach ($roles as $role){
+                                    if ($role == $row['role']){
+                                        echo '<option value="'.$role.'" selected>'.ucfirst($role).'</option>';
+                                    }
+                                    else{
+                                        echo '<option value="'.$role.'">'.ucfirst($role).'</option>';
+                                    } 
                                 }
-                            }
                             ?>
-                            <a class="dropdown-item" href=""
-                                ><i class="bx bx-edit-alt me-1"></i> Edit</a
-                            >
-                            <a class="dropdown-item" href=""
-                                ><i class="bx bx-trash me-1"></i> Delete</a
-                            >
-                            </div>
-                        </div>
+                        </select>
+                        <span class="error-message" style="color: red;">
+                            <?php
+                                if(isset($_SESSION['role_upd_err'])){
+                                    echo $_SESSION['role_upd_err'];
+                                    unset($_SESSION['role_upd_err']);}
+                            ?>
+                        </span>
                     </td>
-                </tr>
+                </div>
+                <div class="form-group mb-3">
+                    <td>
+                        <select name="verified" id="verified">
+                            <?php
+                                if (!empty($row['verified'])){
+                                    echo '<option value="yes" selected>Yes</option>
+                                        <option value="">No</option>';
+                                }
+                                else{
+                                    echo '<option value="yes">Yes</option>
+                                        <option value="" selected>No</option>';
+                                }
+                            ?>
+                        </select>
+                        <span class="error-message" style="color: red;">
+                            <?php
+                                if(isset($_SESSION['verified_err'])){
+                                    echo $_SESSION['verified_err'];
+                                    unset($_SESSION['verified_err']);}
+                            ?>
+                        </span>
+                    </td>
+                </div>
+                <div class="form-group">
+                    <td class="text-center">
+                        <button type="submit" name="update" class="btn btn-primary">Update</button>
+                        <a class="btn btn-primary" href="<?php echo $_SERVER['PHP_SELF'] ?>" role="button">Cancel</a>
+                    </td>
+                </div>
+            </form>
+        </tr>
+
+        <?php
+    }
+    else if (isset($_GET['delete'])){
+        ?>
+            <td><?php echo $row['role'] ?></td>
+            <td><?php echo $row['verified'] ?></td>
+
+                <form action="logic/delete_user.php" method="post">
+                    <div class="form-group mb-3">
+                        <input type="hidden" id="user_to_delete" name="user_to_delete" value="<?php echo $row['username'] ?>">
+                    </div>
+                    <div class="form-group">
+                        <td class="text-center">
+                            <button type="submit" name="update" class="btn btn-primary">Delete</button>
+                            <a class="btn btn-primary" href="<?php echo $_SERVER['PHP_SELF'] ?>" role="button">Cancel</a>
+                        </td>
+                    </div>
+                </form>
+
+        </tr>
+
+        <?php
+    }
+    else{
+        echo '<td>'.$row['role'].'</td>';
+        echo '<td>'.$row['verified'].'</td>';
+
+        ?>
+            <td>
+                <div class="dropdown">
+                    <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                    <i class="bi bi-three-dots-vertical"></i>
+                    </button>
+                    <div class="dropdown-menu">
+                    
+                    <a class="dropdown-item" href="<?php echo 'dashboard_page.php?page='.$page.'&update=true&usr='.$row['username'] ?>"
+                        ><i class="bx bx-edit-alt me-1"></i> Edit</a>
+                    <a class="dropdown-item" href="<?php echo 'dashboard_page.php?page='.$page.'&delete=true&usr='.$row['username'] ?>">
+                        <i class="bx bx-trash me-1"></i> Delete</a>
+                    </div>
+                </div>
+            </td>
+        </tr>
 
     <?php
+    }
 }
 ?>
             </tbody>
