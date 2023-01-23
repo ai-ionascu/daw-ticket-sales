8c7a8b185d673c2f5ab064e724f571fbb708e399
ai_ionascu <ionutionascu27@yahoo.com>
2023-01-23 10:46:12 +0100

    changed db settings

--- conf/db_con.php
+++ conf/db_con.php
@@ -1,9 +1,19 @@
 <?php
 require_once('config.php');
-$servername = $config["SERVER_NAME"];
-$username = $config["DB_USER"];
-$password = $config["DB_PASS"];
-$db_name = $config["DB_NAME"];
+
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$servername = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$db_name = ltrim($dbparts['path'],'/');
+
+// $servername = $config["SERVER_NAME"];
+// $username = $config["DB_USER"];
+// $password = $config["DB_PASS"];
+// $db_name = $config["DB_NAME"];
+
 $con = mysqli_connect($servername, $username, $password, $db_name);
 if (mysqli_connect_errno()) {
   echo "DB connection failed: " . mysqli_connect_error();
@@ -11,4 +21,6 @@ if (mysqli_connect_errno()) {
 }
 
 mysqli_select_db($con, "ticket_sales");
+// mysqli_select_db($con, "t83g94b0z25s43zs");
+
 ?>
