eb4c25eb35512297c74e6a296fc26760ef1c8ca2
ai_ionascu <ionutionascu27@yahoo.com>
2023-01-23 15:20:20 +0100

    homepage upd

--- index.php
+++ index.php
@@ -3,10 +3,22 @@ session_start();
 $page_title = "Home Page";
 include('include/head.php');
 include('include/header.php') ;
+include(dirname(__FILE__).'/conf/db_con.php');
 ?>
 
 <h1>Home Page</h1>
+
 <?php
+$users_query = 'SELECT * FROM users';
+$users_query_run = mysqli_query($con, $users_query);
+if ($users_query_run){
+    foreach($users_query_run as $user){
+        print_r($user);
+    }
+}
+else{
+    echo "Query error.";
+}
 include('include/alerts.php');
 include('include/tail.php') 
 ?>
