937f77e6c2731d6757481cdb22dedc3c93ed22d2
ai_ionascu <ionutionascu27@yahoo.com>
2023-01-31 15:51:52 +0100

    added delete user functionality in admin panel

---
+++ logic/delete_user.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+require_once('../conf/db_con.php');
+
+if(isset($_POST['delete'])){
+
+    $user_id = $_POST['user_to_delete'];
+    $username = $_POST['username_to_delete'];
+
+    if ($username == $_SESSION['username']){
+        $_SESSION['error'] = "You cannot delete your own user.";
+    }
+    else{
+        $delete_query = mysqli_query($con, "DELETE FROM users WHERE id='$user_id'");
+        if(true){
+            $_SESSION['success'] = 'User '.$username.' has been sucessfully deleted.';
+        } else {
+            $_SESSION['error'] = "Error: " . mysqli_error($con);
+        }
+    }
+    
+    $p = $_POST['page'];
+    header("Location: ../dashboard_page.php?page=$p");
+}
+?>
